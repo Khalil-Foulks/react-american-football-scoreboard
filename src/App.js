46e4814862bb36451209b05f6ea3a1d6b6a56c9d
Khalil <khalilfoulks@gmail.com>
2020-06-08 18:30:57 -0400

    created a working TopRow component file, created working quarters button, still working on buttons component file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, {useState} from "react"
 import BottomRow from "./BottomRow";
+import TopRow from "./TopRow";
+import Buttons from "./BottomRow"
 import "./App.css";
 
-function App(props) {
+function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [scoreHome, setScoreHome] = useState(21)
-  const [scoreAway, setScoreAway] = useState(14)
+  const [scoreHome, setScoreHome] = useState(0)
+  const [scoreAway, setScoreAway] = useState(0)
+  const [quarter, setQuarter] = useState(1)
 
   const touchdownHome = evt =>{
     setScoreHome(scoreHome + 7)
@@ -24,24 +27,16 @@ function App(props) {
     setScoreAway(scoreAway + 3)
   }
 
+  const quarters = evt =>{
+    quarter === 4 ? setQuarter(quarter - 3) : setQuarter(quarter + 1)
+  }
+
 
   return (
     <div className="container">
       <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-          <div className="home__score">{ scoreHome }</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{ scoreAway }</div>
-          </div>
-        </div>
-        <BottomRow />
+        <TopRow scoreHome ={scoreHome} scoreAway={scoreAway} homeName={'Patriots'} awayName ={'Eagles'}/>
+        <BottomRow quarter={quarter}/>
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -53,6 +48,9 @@ function App(props) {
         <div className="awayButtons">
           <button onClick={touchdownAway} className="awayButtons__touchdown">Away Touchdown</button>
           <button onClick={fieldGoalAway} className="awayButtons__fieldGoal">Away Field Goal</button>
+        </div>
+        <div className="quarterButtons">
+          <button onClick={ quarters } className="awayButtons__touchdown">Change Quarter</button>
         </div>
       </section>
     </div>
---
+++ src/Buttons.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+
+const Buttons = props => {
+
+    return(
+        <div className="homeButtons">
+
+          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+          <button onClick={props.touchdownHome} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={props.fieldGoalHome} className="homeButtons__fieldGoal">Home Field Goal</button>
+        </div>
+        <div className="awayButtons">
+          <button onClick={props.touchdownAway} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={props.fieldGoalAway} className="awayButtons__fieldGoal">Away Field Goal</button>
+        </div>
+        <div className="quarterButtons">
+          <button onClick={ props.quarters } className="awayButtons__touchdown">Change Quarter</button>
+        </div>
+        
+    );
+};
+
+export default Buttons;
